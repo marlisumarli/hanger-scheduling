03e5898f4390ec79e46b76e1f0db9491d23c4cce
Marleess Windows <marleysumarly@gmail.com>
2022-11-02 00:19:51 +0700

    template

--- public/index.php
+++ public/index.php
@@ -4,9 +4,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Subjig\Report\App\Router;
 use Subjig\Report\Config\Database;
+use Subjig\Report\Controller\AdminHomeController;
 
 Database::getConnection('prod');
 
-//
+Router::get('GET', '/admin/dashboard', AdminHomeController::class, 'index');
 
 Router::run();
