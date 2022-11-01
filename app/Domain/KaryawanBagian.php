cfece9545a0a3569c5223e3b034325ce38b7c1a3
Marleess Windows <marleysumarly@gmail.com>
2022-11-02 00:19:01 +0700

    karyawan bagian repository

---
+++ app/Domain/KaryawanBagian.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Subjig\Report\Domain;
+
+class KaryawanBagian
+{
+    public int $bagianId;
+    public string $name;
+    public string $createdAt;
+    public ?string $updatedAt = null;
+
+}
