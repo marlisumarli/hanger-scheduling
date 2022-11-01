5de37700b1c4d9fef3b07b9ee7665567906a5a23
Marleess Windows <marleysumarly@gmail.com>
2022-11-01 10:55:23 +0700

    Setup database overhauled

--- Database.sql
+++ Database.sql
@@ -2,232 +2,190 @@ CREATE SCHEMA subjig_report;
 CREATE SCHEMA subjig_report_test;
 CREATE SCHEMA kotretan;
 
-# CREATE TABLE karyawans(
-#   username VARCHAR(55) PRIMARY KEY,
-#   password VARCHAR(255) NOT NULL ,
-#   created_at DATE NOT NULL ,
-#
-# );
-
-CREATE TABLE subjig_k2f_supply
+CREATE TABLE karyawans
 (
-    id                  VARCHAR(55) PRIMARY KEY,
-    supply_tanggal      DATE       NOT NULL,
-    line_a_spdmt_a      INTEGER(3) NULL,
-    line_a_spdmt_b      INTEGER(3) NULL,
-    line_a_front_r      INTEGER(3) NULL,
-    line_a_front_l      INTEGER(3) NULL,
-    line_a_front_top    INTEGER(3) NULL,
-    line_a_fender       INTEGER(3) NULL,
-    line_a_under_rl     INTEGER(3) NULL,
-    line_a_body_rl      INTEGER(3) NULL,
-    line_a_center_upper INTEGER(3) NULL,
-    line_a_inner_upper  INTEGER(3) NULL,
-    line_a_inner        INTEGER(3) NULL,
-    line_b_spdmt_a      INTEGER(3) NULL,
-    line_b_spdmt_b      INTEGER(3) NULL,
-    line_b_front_r      INTEGER(3) NULL,
-    line_b_front_l      INTEGER(3) NULL,
-    line_b_front_top    INTEGER(3) NULL,
-    line_b_fender       INTEGER(3) NULL,
-    line_b_under_rl     INTEGER(3) NULL,
-    line_b_body_rl      INTEGER(3) NULL,
-    line_b_center_upper INTEGER(3) NULL,
-    line_b_inner_upper  INTEGER(3) NULL,
-    line_b_inner        INTEGER(3) NULL,
-    line_c_spdmt_a      INTEGER(3) NULL,
-    line_c_spdmt_b      INTEGER(3) NULL,
-    line_c_front_r      INTEGER(3) NULL,
-    line_c_front_l      INTEGER(3) NULL,
-    line_c_front_top    INTEGER(3) NULL,
-    line_c_fender       INTEGER(3) NULL,
-    line_c_under_rl     INTEGER(3) NULL,
-    line_c_body_rl      INTEGER(3) NULL,
-    line_c_center_upper INTEGER(3) NULL,
-    line_c_inner_upper  INTEGER(3) NULL,
-    line_c_inner        INTEGER(3) NULL
-) ENGINE = InnoDB
-  CHARSET = utf8;
+    username           VARCHAR(55) PRIMARY KEY,
+    password           VARCHAR(255) NOT NULL,
+    created_at         DATETIME     NOT NULL,
+    update_password_at DATETIME     NOT NULL,
+    online_status   BOOLEAN NOT NULL ,
+    last_login DATETIME NOT NULL
+)ENGINE = InnoDB
+ CHARSET = utf8mb4;
 
-CREATE TABLE subjig_k2vg_supply
+CREATE TABLE karyawan_details
 (
-    id               VARCHAR(55) PRIMARY KEY,
-    supply_tanggal   DATE       NOT NULL,
-    line_a_cht       INTEGER(3) NULL,
-    line_a_chf       INTEGER(3) NULL,
-    line_a_front_r   INTEGER(3) NULL,
-    line_a_front_l   INTEGER(3) NULL,
-    line_a_front_top INTEGER(3) NULL,
-    line_a_rack      INTEGER(3) NULL,
-    line_a_body_r    INTEGER(3) NULL,
-    line_a_body_l    INTEGER(3) NULL,
-    line_b_cht       INTEGER(3) NULL,
-    line_b_chf       INTEGER(3) NULL,
-    line_b_front_r   INTEGER(3) NULL,
-    line_b_front_l   INTEGER(3) NULL,
-    line_b_front_top INTEGER(3) NULL,
-    line_b_rack      INTEGER(3) NULL,
-    line_b_body_r    INTEGER(3) NULL,
-    line_b_body_l    INTEGER(3) NULL,
-    line_c_cht       INTEGER(3) NULL,
-    line_c_chf       INTEGER(3) NULL,
-    line_c_front_r   INTEGER(3) NULL,
-    line_c_front_l   INTEGER(3) NULL,
-    line_c_front_top INTEGER(3) NULL,
-    line_c_rack      INTEGER(3) NULL,
-    line_c_body_r    INTEGER(3) NULL,
-    line_c_body_l    INTEGER(3) NULL
-) ENGINE = InnoDB
-  CHARSET = utf8;
+    username           VARCHAR(55) PRIMARY KEY,
+    name           VARCHAR(55) NOT NULL,
+    bagian_id INTEGER NOT NULL
+)ENGINE = InnoDB
+ CHARSET = utf8mb4;
 
-CREATE TABLE subjig_k2f_perbaikan
-(
-    id              VARCHAR(55) PRIMARY KEY,
-    tanggal_op      DATE        NOT NULL,
-    op_spdmt_a      INTEGER(3)  NULL,
-    op_spdmt_b      INTEGER(3)  NULL,
-    op_front_r      INTEGER(3)  NULL,
-    op_front_l      INTEGER(3)  NULL,
-    op_front_top    INTEGER(3)  NULL,
-    op_fender       INTEGER(3)  NULL,
-    op_under_rl     INTEGER(3)  NULL,
-    op_body_rl      INTEGER(3)  NULL,
-    op_center_upper INTEGER(3)  NULL,
-    op_inner_upper  INTEGER(3)  NULL,
-    op_inner        INTEGER(3)  NULL,
-    tanggal_cl      DATE        NULL,
-    cl_spdmt_a      INTEGER(3)  NULL,
-    cl_spdmt_b      INTEGER(3)  NULL,
-    cl_front_r      INTEGER(3)  NULL,
-    cl_front_l      INTEGER(3)  NULL,
-    cl_front_top    INTEGER(3)  NULL,
-    cl_fender       INTEGER(3)  NULL,
-    cl_under_rl     INTEGER(3)  NULL,
-    cl_body_rl      INTEGER(3)  NULL,
-    cl_center_upper INTEGER(3)  NULL,
-    cl_inner_upper  INTEGER(3)  NULL,
-    cl_inner        INTEGER(3)  NULL,
-    pic             VARCHAR(55) NULL,
-    keterangan      VARCHAR(55) NULL
-) ENGINE = InnoDB
-  CHARSET = utf8;
-
-CREATE TABLE subjig_k2vg_perbaikan
-(
-    id           VARCHAR(55) PRIMARY KEY,
-    tanggal_op   DATE        NOT NULL,
-    op_cht       INTEGER(3)  NULL,
-    op_chf       INTEGER(3)  NULL,
-    op_front_r   INTEGER(3)  NULL,
-    op_front_l   INTEGER(3)  NULL,
-    op_front_top INTEGER(3)  NULL,
-    op_rack      INTEGER(3)  NULL,
-    op_body_r    INTEGER(3)  NULL,
-    op_body_l    INTEGER(3)  NULL,
-    tanggal_cl   DATE        NULL,
-    cl_cht       INTEGER(3)  NULL,
-    cl_chf       INTEGER(3)  NULL,
-    cl_front_r   INTEGER(3)  NULL,
-    cl_front_l   INTEGER(3)  NULL,
-    cl_front_top INTEGER(3)  NULL,
-    cl_rack      INTEGER(3)  NULL,
-    cl_body_r    INTEGER(3)  NULL,
-    cl_body_l    INTEGER(3)  NULL,
-    pic          VARCHAR(55) NULL,
-    keterangan   VARCHAR(55) NULL
-) ENGINE = InnoDB
-  CHARSET = utf8;
-
-CREATE TABLE subjig_k2f_cuci
-(
-    id              VARCHAR(55) PRIMARY KEY,
-    tanggal_op      DATE        NOT NULL,
-    op_spdmt_a      INTEGER(3)  NULL,
-    op_spdmt_b      INTEGER(3)  NULL,
-    op_front_r      INTEGER(3)  NULL,
-    op_front_l      INTEGER(3)  NULL,
-    op_front_top    INTEGER(3)  NULL,
-    op_fender       INTEGER(3)  NULL,
-    op_under_rl     INTEGER(3)  NULL,
-    op_body_rl      INTEGER(3)  NULL,
-    op_center_upper INTEGER(3)  NULL,
-    op_inner_upper  INTEGER(3)  NULL,
-    op_inner        INTEGER(3)  NULL,
-    tanggal_cl      DATE        NULL,
-    cl_spdmt_a      INTEGER(3)  NULL,
-    cl_spdmt_b      INTEGER(3)  NULL,
-    cl_front_r      INTEGER(3)  NULL,
-    cl_front_l      INTEGER(3)  NULL,
-    cl_front_top    INTEGER(3)  NULL,
-    cl_fender       INTEGER(3)  NULL,
-    cl_under_rl     INTEGER(3)  NULL,
-    cl_body_rl      INTEGER(3)  NULL,
-    cl_center_upper INTEGER(3)  NULL,
-    cl_inner_upper  INTEGER(3)  NULL,
-    cl_inner        INTEGER(3)  NULL,
-    keterangan      VARCHAR(55) NULL
-) ENGINE = InnoDB
-  CHARSET = utf8;
-
-CREATE TABLE subjig_k2vg_cuci
-(
-    id           VARCHAR(55) PRIMARY KEY,
-    tanggal_op   DATE        NOT NULL,
-    op_cht       INTEGER(3)  NULL,
-    op_chf       INTEGER(3)  NULL,
-    op_front_r   INTEGER(3)  NULL,
-    op_front_l   INTEGER(3)  NULL,
-    op_front_top INTEGER(3)  NULL,
-    op_rack      INTEGER(3)  NULL,
-    op_body_r    INTEGER(3)  NULL,
-    op_body_l    INTEGER(3)  NULL,
-    tanggal_cl   DATE        NULL,
-    cl_cht       INTEGER(3)  NULL,
-    cl_chf       INTEGER(3)  NULL,
-    cl_front_r   INTEGER(3)  NULL,
-    cl_front_l   INTEGER(3)  NULL,
-    cl_front_top INTEGER(3)  NULL,
-    cl_rack      INTEGER(3)  NULL,
-    cl_body_r    INTEGER(3)  NULL,
-    cl_body_l    INTEGER(3)  NULL,
-    keterangan   VARCHAR(55) NULL
-) ENGINE = InnoDB
-  CHARSET = utf8;
-
-CREATE TABLE subjig_k2f
+CREATE TABLE karyawan_bagians
 (
     id          INTEGER PRIMARY KEY AUTO_INCREMENT,
-    subjig_nama VARCHAR(55)            NOT NULL,
-    qty         INTEGER(2)             NOT NULL,
-    stok_gudang INTEGER(4)             NOT NULL,
-    stok_line   INTEGER(4)             NOT NULL,
-    jumlah_cuci INTEGER(4)             NOT NULL,
-    perbaikan   INTEGER(3)             NOT NULL
+    name           VARCHAR(55) NOT NULL
+)ENGINE = InnoDB
+ CHARSET = utf8mb4;
 
-) ENGINE = InnoDB
-  CHARSET = utf8;
+CREATE TABLE sessions
+(
+    username           VARCHAR(55) PRIMARY KEY,
+    session_id           VARCHAR(120) NOT NULL
+)ENGINE = InnoDB
+ CHARSET = utf8mb4;
 
-CREATE TABLE subjig_k2vg
+
+CREATE TABLE log
 (
     id          INTEGER PRIMARY KEY AUTO_INCREMENT,
-    subjig_nama VARCHAR(55)            NOT NULL,
-    qty         INTEGER(2)             NOT NULL,
-    stok_gudang INTEGER(4)             NOT NULL,
-    stok_line   INTEGER(4)             NOT NULL,
-    jumlah_cuci INTEGER(4)             NOT NULL,
-    perbaikan   INTEGER(3)             NOT NULL
+    username           VARCHAR(55) NOT NULL,
+    action           VARCHAR(55) NOT NULL,
+    action_at         DATETIME     NOT NULL
 
 ) ENGINE = InnoDB
-  CHARSET = utf8;
+  CHARSET = utf8mb4;
 
-ALTER TABLE subjig_k2f_supply
-    ADD COLUMN supply_target_line_c INTEGER(3) AFTER supply_tanggal,
-    ADD COLUMN supply_target_line_b INTEGER(3) AFTER supply_tanggal,
-    ADD COLUMN supply_target_line_a INTEGER(3) AFTER supply_tanggal;
-ALTER TABLE subjig_k2vg_supply
-    ADD COLUMN supply_target_line_c INTEGER(3) AFTER supply_tanggal,
-    ADD COLUMN supply_target_line_b INTEGER(3) AFTER supply_tanggal,
-    ADD COLUMN supply_target_line_a INTEGER(3) AFTER supply_tanggal;
+CREATE TABLE category
+(
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name      VARCHAR(55)       NOT NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
 
-DROP TABLE subjig_k2vg;
-DROP TABLE subjig_k2f;
+CREATE TABLE category_surat_jalan
+(
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name      VARCHAR(55)       NOT NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE k2f
+(
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(55) NOT NULL ,
+    qty INT NOT NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE k1a
+(
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(55) NOT NULL ,
+    qty INT NOT NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE mainjig
+(
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(55) NOT NULL ,
+    qty INT NOT NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE messboat
+(
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(55) NOT NULL ,
+    qty INT NOT NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE tx_k2f
+(
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    category_id INT NOT NULL ,
+    type VARCHAR(10) NOT NULL ,
+    year DATE NULL ,
+    speedometer_a INT NULL ,
+    speedometer_b INT NULL ,
+    ring_speedometer INT NULL ,
+    front_r INT NULL ,
+    front_l INT NULL ,
+    front_top INT NULL ,
+    fender INT NULL ,
+    under_rL INT NULL ,
+    lid_pocket INT NULL ,
+    body_rl INT NULL ,
+    center_upper INT NULL ,
+    rr_center INT NULL ,
+    center INT NULL ,
+    inner_upper INT NULL ,
+    cinner INT NULL ,
+    supply_tanggal DATE NULL ,
+    perbaikan_tanggal DATE NULL ,
+    selesai_tanggal  DATE NULL,
+        pic VARCHAR(55) NULL,
+        datang_tanggal DATE NULL,
+        keterangan VARCHAR(55) NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE tx_k1a
+(
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    category_id INT NOT NULL ,
+    type VARCHAR(10) NOT NULL ,
+    year DATE NULL ,
+    chf  INT NULL ,
+        spdmt  INT NULL,
+        body_r INT NULL,
+        body_l  INT NULL ,
+        fender INT NULL,
+    supply_tanggal DATE NULL ,
+    perbaikan_tanggal DATE NULL ,
+    selesai_tanggal  DATE NULL,
+    pic VARCHAR(55) NULL,
+    datang_tanggal DATE NULL,
+    keterangan VARCHAR(55) NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE tx_mainjig
+(
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    category_id INT NOT NULL ,
+    type VARCHAR(10) NOT NULL ,
+    year DATE NULL ,
+    jumlah INT NULL ,
+    supply_tanggal DATE NULL ,
+    perbaikan_tanggal DATE NULL ,
+    selesai_tanggal  DATE NULL,
+    pic VARCHAR(55) NULL,
+    datang_tanggal DATE NULL,
+    keterangan VARCHAR(55) NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE tx_messboat
+(
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    category_id INT NOT NULL ,
+    type VARCHAR(10) NOT NULL ,
+    year DATE NULL ,
+    jumlah INT NULL ,
+    supply_tanggal DATE NULL ,
+    perbaikan_tanggal DATE NULL ,
+    selesai_tanggal  DATE NULL,
+    pic VARCHAR(55) NULL,
+    datang_tanggal DATE NULL,
+    keterangan VARCHAR(55) NULL
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
+
+CREATE TABLE tx_surat_jalan
+(
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    category_id INT NOT NULL ,
+    tanggal DATE NOT NULL ,
+    k2f INT,
+    k1a INT ,
+    mainjig_line_a INT,
+    mainjig_line_b INT,
+    mainjig_line_c INT ,
+    messboat_line_a INT ,
+    messboat_line_b INT ,
+    messboat_line_c INT
+) ENGINE = InnoDB
+  CHARSET = utf8mb4;
